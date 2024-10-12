40935041e3e397f77e3d7735d621a3fea31a9aa3
Daniel Scislowski <mr.scislowski@gmail.com>
2024-10-12 18:31:32 -0500

    add vitest test on Todo component

---
+++ todo-app/todo-frontend/src/Todos/Todo.test.jsx
@@ -0,0 +1,19 @@
+import { test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Todo from './Todo'
+
+test('renders content', () => {
+  const todo = {
+    text: 'Test whether the Todo component renders content',
+    done: false
+  }
+
+  render(<Todo todo={todo} deleteTodo={() => 0} completeTodo={() => 0} />)
+
+  const elementText = screen.getByText('Test whether the Todo component renders content')
+  const elementCompletion = screen.getByText('This todo is not done')
+  // const elementCompletion = screen.getByText('This todo is done')
+
+  expect(elementText).toBeDefined()
+  expect(elementCompletion).toBeDefined()
+})
